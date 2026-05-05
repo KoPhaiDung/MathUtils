53a1e63021bcf2fedd783b5b8e0ad80945e2da4b
KoPhaiDung <dungvqht1@gmail.com>
2026-05-05 11:36:01 +0700

    test

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -8,18 +8,12 @@ import java.io.File;
 public class PathTest {
     private static Logger logger = LoggerFactory.getLogger(MathUtilsTest.class);
     @Test
-    public void testFilePath() {
-
-        // Cố tình dùng định dạng cứng của Windows: "target\classes"
-        String expectedPath = "target\\classes";
+    public void testFilePathRefactored() {
+        // Tự động lấy dấu \ hoặc / tùy theo OS đang chạy
+        String separator = File.separator;
+        String expectedPath = "target" + separator + "classes";
 
         File file = new File("target/classes");
-        String actualPath = file.getPath();
-
-        logger.info("Hệ điều hành: " + System.getProperty("os.name"));
-        logger.info("Đường dẫn thực tế: " + actualPath);
-
-        // Test này sẽ PASS trên Windows nhưng FAIL trên Ubuntu và macOS
-        assertTrue(actualPath.contains(expectedPath), "Đường dẫn không khớp với định dạng hệ điều hành!");
+        assertTrue(file.getPath().contains(expectedPath));
     }
 }

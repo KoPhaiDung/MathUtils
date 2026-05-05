c1ec2f484cbc2f066d2a7ff7e9d017bf36f650de
KoPhaiDung <dungvqht1@gmail.com>
2026-05-05 11:24:10 +0700

    2

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -1,19 +1,23 @@
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 
 public class PathTest {
-
+    private static Logger logger = LoggerFactory.getLogger(MathUtilsTest.class);
     @Test
     public void testFilePath() {
+
         // Cố tình dùng định dạng cứng của Windows: "target\classes"
         String expectedPath = "target\\classes";
 
         File file = new File("target/classes");
         String actualPath = file.getPath();
 
-        System.out.println("Hệ điều hành: " + System.getProperty("os.name"));
-        System.out.println("Đường dẫn thực tế: " + actualPath);
+        logger.info("Hệ điều hành: " + System.getProperty("os.name"));
+        logger.info("Đường dẫn thực tế: " + actualPath);
 
         // Test này sẽ PASS trên Windows nhưng FAIL trên Ubuntu và macOS
         assertTrue(actualPath.contains(expectedPath), "Đường dẫn không khớp với định dạng hệ điều hành!");
